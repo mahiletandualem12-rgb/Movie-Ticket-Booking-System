c8dfa07e52908e40f0fa394d9514cdab898fb970
nathnaelel <nathnaelel@gmail.com>
2026-01-19 21:10:33 +0300

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -1,76 +1,59 @@
 #include <iostream>
 #include <string>
-#include "menu.h"
 using namespace std;
 
-// External data
+/* ===== EXTERN GLOBALS ===== */
 extern const int MOVIE_COUNT;
 extern const int SEATS_PER_MOVIE;
+
 extern string movies[];
 extern string showTimes[];
 extern int durations[];
 extern int seats[][20];
 extern int ticketPrice[];
 
-// Functions
-
+/* ===== FUNCTION PROTOTYPES ===== */
 void initializeSeats(int (*)[20]);
 void showMovies(string*, string*, int*);
 void showSeats(int (*)[20], int);
 
 void bookSeat(int (*)[20], int, int);
+void cancelSeat(int (*)[20], int, int);
 void updateBooking(int (*)[20], int);
 
+int countBookedSeats(int (*)[20], int);
+void bookTicket();
+int getTicketPrice(int *, int);
 void showBill(string*, int*, int, int);
 
-// Helper
+/* ===== MAIN ===== */
+int main() {
+    initializeSeats(seats);
+    int choice;
 
-int countBookedSeats(int (*seatPtr)[20], int movieIndex) {
-    int count = 0;
-    for (int i = 0; i < 20; i++)
-        if (seatPtr[movieIndex][i] == 0)
-            count++;
-    return count;
-}
+    do {
+        cout << "\n========== MOVIE TICKET SYSTEM ==========\n";
+        cout << "1. Show Movies\n";
+        cout << "2. Book Ticket\n";
+        cout << "3. Exit\n";
+        cout << "Choose option: ";
+        cin >> choice;
 
-// Booking Flow
-
-void bookTicket() {
-    int movieChoice, seatCount, seatNo;
-
-    showMovies(movies, showTimes, durations);
-
-    cout << "Choose movie (1-5): ";
-    cin >> movieChoice;
-    movieChoice--;
-
-    showSeats(seats, movieChoice);
-
-    cout << "Seats to book: ";
-    cin >> seatCount;
-
-    for (int i = 0; i < seatCount; i++) {
-        cout << "Seat number: ";
-        cin >> seatNo;
-
-        int idx = seatNo - 1;
-        if (seats[movieChoice][idx] == 1) {
-            bookSeat(seats, movieChoice, idx);
-        } else {
-            cout << "Already booked.\n";
-            i--;
+        switch (choice) {
+            case 1:
+                showMovies(movies, showTimes, durations);
+                break;
+            case 2:
+                bookTicket();
+                break;
+            case 3:
+                cout << "Thank you for using the system.\n";
+                break;
+            default:
+                cout << "Invalid choice. Please try again.\n";
         }
-    }
 
-    char ch;
-    cout << "Update booking? (y/n): ";
-    cin >> ch;
+    } while (choice != 3);
 
-    if (ch == 'y' || ch == 'Y')
-        updateBooking(seats, movieChoice);
-
-    int finalSeats = countBookedSeats(seats, movieChoice);
-    showBill(movies, ticketPrice, movieChoice, finalSeats);
+    return 0;
 }
-
-
